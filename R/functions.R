4198dd82de301685f57944f27e42eecc433c4176
Garrick Aden-Buie <garrick@adenbuie.com>
2020-11-18 10:45:55 -0500

    simplify black_box()

--- R/functions.R
+++ R/functions.R
@@ -1,9 +1,5 @@
-black_box <- function(always_be_nice = FALSE, inclination_to_be_nice = 0.1) {
-  random_number <- runif(1)
-
-  want_to_be_nice <- random_number <= inclination_to_be_nice
-
-  be_nice_this_time <- isTRUE(always_be_nice) || be_nice_this_time
+black_box <- function(nice_probability = 0.25) {
+  be_nice_this_time <- runif(1) >= nice_probability
 
   if (be_nice_this_time) {
     message(praise::praise("(ʘ‿ʘ)╯ You are ${adjective}!"))
